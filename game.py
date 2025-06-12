7abf00a0b11d3e11f6afa26d034b9c3ec32a6e8a
Edward Tladi <Etladi@csir.co.za>
2025-06-12 18:19:34 +0200

    windows

--- game.py
+++ game.py
@@ -1,10 +1,16 @@
-import pygame, sys, random
+#import pygame, sys, random
+#from pygame import draw
+
+import pygame
+import sys
+import random
 from pygame import draw
+
 
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 480, 480
 GRIDSIZE=20
-GRID_WIDTH = SCREEN_HEIGHT/GRIDSIZE
-GRID_HEIGHT = SCREEN_WIDTH/GRIDSIZE
+GRID_WIDTH = SCREEN_HEIGHT//GRIDSIZE
+GRID_HEIGHT = SCREEN_WIDTH//GRIDSIZE
 UP =(0,-1)
 DOWN=(0, 1)
 LEFT=(-1,0)
